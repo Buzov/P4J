0f344b1f55f345b3982c1de6193c17b668d7f604
artur <artur@kari.cs_ltd.intranet>
2016-01-13 16:42:41 +0200

    added mail

--- src/main/java/sql/create_scheme.sql
+++ src/main/java/sql/create_scheme.sql
@@ -1,6 +1,6 @@
 CREATE SCHEMA `p4j` DEFAULT CHARACTER SET utf8;
 
-CREATE TABLE `p4j`.`login_password` (
+CREATE TABLE if not exists `p4j`.`login_password` (
   `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
   `login` NVARCHAR(50) NOT NULL,
   `password` NVARCHAR(50) NOT NULL,
@@ -8,8 +8,12 @@ CREATE TABLE `p4j`.`login_password` (
    FOREIGN KEY(comment_id) REFERENCES comment(id),
 );
 
-CREATE TABLE `p4j`.`comment` (
+CREATE TABLE if not exists `p4j`.`comment` (
   `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
   `comment` NVARCHAR(50) NOT NULL,
 );
 
+CREATE TABLE if not exists `p4j`.`status` (
+  `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  `comment` NVARCHAR(50) NOT NULL,
+);
